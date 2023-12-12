a856588faf437b9288a45afbeab3fced8c5090ce
TP-Bluray <TP-Bluray@DESKTOP-F8QQ28U>
2023-12-12 13:08:02 +0100

    Intégration de l'API Personne et retrait des traces de code de l'API Support

---
+++ VideoTheque/ViewModels/PersonneViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using VideoTheque.DTOs;
+
+namespace VideoTheque.ViewModels
+{
+    public class PersonneViewModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("nom")]
+        [Required]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("prenom")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("nationalite")]
+        [Required]
+        public string Nationality { get; set; }
+
+        [JsonPropertyName("date-naissance")]
+        [Required]
+        public DateTime BirthDay { get; set; }
+
+        [JsonPropertyName("nom-prenom")]
+        [Required]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
